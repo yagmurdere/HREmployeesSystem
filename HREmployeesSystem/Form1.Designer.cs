31c3bad2c438477769dd4276e3a0ca487426f341
Yagmur Dere <127977948+yagmurdere@users.noreply.github.com>
2024-08-05 16:11:23 +0300

    Add project files.

---
+++ HREmployeesSystem/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace HREmployeesSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Giriş = new System.Windows.Forms.Button();
+            this.Çıkış = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HREmployeesSystem.Properties.Resources.ispark_logo_square;
+            this.pictureBox1.Location = new System.Drawing.Point(39, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(319, 291);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(365, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kullanıcı Adı";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(410, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Şifre";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(486, 129);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(486, 165);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(185, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Giriş
+            // 
+            this.Giriş.BackColor = System.Drawing.Color.YellowGreen;
+            this.Giriş.Location = new System.Drawing.Point(486, 231);
+            this.Giriş.Name = "Giriş";
+            this.Giriş.Size = new System.Drawing.Size(81, 49);
+            this.Giriş.TabIndex = 5;
+            this.Giriş.Text = "button1";
+            this.Giriş.UseVisualStyleBackColor = false;
+            // 
+            // Çıkış
+            // 
+            this.Çıkış.BackColor = System.Drawing.Color.Red;
+            this.Çıkış.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Çıkış.Location = new System.Drawing.Point(590, 231);
+            this.Çıkış.Name = "Çıkış";
+            this.Çıkış.Size = new System.Drawing.Size(81, 49);
+            this.Çıkış.TabIndex = 6;
+            this.Çıkış.Text = "button2";
+            this.Çıkış.UseVisualStyleBackColor = false;
+            this.Çıkış.Click += new System.EventHandler(this.Çıkış_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.Giriş;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.Çıkış;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Çıkış);
+            this.Controls.Add(this.Giriş);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Giriş;
+        private System.Windows.Forms.Button Çıkış;
+    }
+}
+
